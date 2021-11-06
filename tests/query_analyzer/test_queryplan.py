e6a013de7b6cf6f5dbcc24302c1fe2838bc96d31
Tan Wei Lun <weilunn.97@gmail.com>
2021-11-06 12:12:33 +0800

    Removed VARY() and streamlined UI

--- tests/query_analyzer/test_queryplan.py
+++ tests/query_analyzer/test_queryplan.py
@@ -4,37 +4,37 @@ from query_analyzer.queryplan import QueryPlan
 
 
 class TestQueryPlan(unittest.TestCase):
-    def setUp(self):
-        self.raw_query_nested = {
-            "Node Type": "Test",
+    def setup(self):
+        self.qep_nested_json = {
+            "Node Type": "T",
             "Total Cost": 10,
             "Plans": [
                 {
-                    "Node Type": "B",
+                    "Node Type": "T1A",
                     "Total Cost": 100,
                     "Plans": [
-                        {"Node Type": "D", "Total Cost": 200, "Plans": []}
+                        {"Node Type": "T1B", "Total Cost": 200, "Plans": []}
                     ],
                 },
-                {"Node Type": "C", "Total Cost": 20, "Plans": []},
+                {"Node Type": "T2", "Total Cost": 20, "Plans": []},
             ],
         }
-        self.raw_query_single = {
-            "Node Type": "Test",
+        self.qep_single_json = {
+            "Node Type": "T",
             "Total Cost": 10,
             "Plans": [],
         }
-        self.query_plan_nested = QueryPlan(self.raw_query_nested, None)
-        self.query_plan_single = QueryPlan(self.raw_query_single, None)
+        self.qep_nested = QueryPlan(self.qep_nested_json, None)
+        self.qep_single = QueryPlan(self.qep_single_json, None)
 
-    def test_construct_graph(self):
-        self.assertEqual(len(self.query_plan_nested.graph.nodes), 4)
-        self.assertEqual(len(self.query_plan_nested.graph.edges), 3)
+    def test_graph_creation(self):
+        self.assertEqual(len(self.qep_nested.graph.nodes), 4)
+        self.assertEqual(len(self.qep_nested.graph.edges), 3)
 
-    def test_construct_graph_single_node(self):
-        self.assertEqual(len(self.query_plan_single.graph.nodes), 1)
-        self.assertEqual(len(self.query_plan_single.graph.edges), 0)
+    def test_graph_single_node_creation(self):
+        self.assertEqual(len(self.qep_single.graph.nodes), 1)
+        self.assertEqual(len(self.qep_single.graph.edges), 0)
 
-    def test_calculate_total_cost(self):
-        self.assertEqual(self.query_plan_nested.calculate_total_cost(), 330)
-        self.assertEqual(self.query_plan_single.calculate_total_cost(), 10)
+    def test_cost_computation(self):
+        self.assertEqual(self.qep_nested.calculate_total_cost(), 330)
+        self.assertEqual(self.qep_single.calculate_total_cost(), 10)
